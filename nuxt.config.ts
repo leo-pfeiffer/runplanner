5e0710bf779ef60fbd54757e4c947b84575552d0
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2023-08-07 09:18:18 +0100

    feat: public runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,12 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
-    stravaClientId: '',
-    stravaClientSecret: '',
-    stravaScope: '',
-    stravaCallbackUrl: '',
-    stravaOauthUrl: '',
+    public: {
+      stravaClientId: process.env.NUXT_STRAVA_CLIENT_ID,
+      stravaClientSecret: process.env.NUXT_STRAVA_CLIENT_SECRET,
+      stravaScope: process.env.NUXT_STRAVA_SCOPE,
+      stravaCallbackUrl: process.env.NUXT_STRAVA_CALLBACK_URL,
+      stravaOauthUrl: process.env.NUXT_STRAVA_OAUTH_URL,
+    },
   }
 })
