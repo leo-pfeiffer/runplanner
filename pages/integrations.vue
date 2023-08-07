93ed9dfac33dfda0324e87782a160f1117957324
leo-pfeiffer <leopold.pfeiffer@icloud.com>
2023-08-07 09:59:34 +0100

    feat: oauth flow done

--- pages/integrations.vue
+++ pages/integrations.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-import { useCookies } from "vue3-cookies";
-const { cookies } = useCookies();
-const config = useRuntimeConfig()
+const config = useRuntimeConfig().public;
 
-const url = config.stravaOauthUrl + "?" + new URLSearchParams({
-  response_type: "code",
-  client_id: `${config.stravaClientId}`,
-  scope: `${config.stravaScope}`,
-  redirect_uri: `${config.stravaCallbackUrl}`
-})
+const hasStravaUser = ref(false);
+const userId = ref(0);
+const stravaRefreshToken = ref("")
+const stravaAccessToken = ref("")
+const theUrl = ref("");
+
+const updateUrl = () => {
+  const url = config.stravaOauthUrl + "?" + new URLSearchParams({
+    response_type: "code",
+    client_id: `${config.stravaClientId}`,
+    scope: `${config.stravaScope}`,
+    redirect_uri: `${config.stravaCallbackUrl}?userId=${userId.value}`,
+  })
+  console.log(url)
+  theUrl.value = url
+}
 
 const makeRequest = async () => {
   console.log("makeRequest, token: ", stravaAccessToken.value)
@@ -23,27 +31,57 @@ const makeRequest = async () => {
   console.log(data)
 }
 
-const stravaAuthCode = ref("")
-const stravaRefreshToken = ref("")
-const stravaAccessToken = ref("")
+const refreshStravaToken = async () => {
+  console.log("refreshStravaToken")
+  const response = await fetch("/api/strava-refresh?" + new URLSearchParams({userId: `${userId.value}`}), {
+    method: "POST"
+  })
+  const data = await response.json()
+  console.log(data)
+}
 
-onMounted(() => {
-  const stravaCode = cookies.get("runplanner-strava-auth-code")
-  const refreshToken = cookies.get("runplanner-strava-refresh-token")
-  const accessToken = cookies.get("runplanner-strava-access-token")
-  if (stravaCode) {
-    stravaAuthCode.value = stravaCode
+const removeStrava = async () => {
+  console.log("removeStrava")
+  const response = await fetch("/api/strava-user?" + new URLSearchParams({userId: `${userId.value}`}), {
+    method: "DELETE"
+  })
+  const data = await response.json()
+  console.log(data)
+  hasStravaUser.value = false
+}
+
+const getUser = async () => {
+  const response = await fetch("/api/user?" + new URLSearchParams({
+    email: "leopold.pfeiffer@icloud.com"
+  }))
+  const data = await response.json()
+  userId.value = data.result.id
+  if (data.result.stravaUser) {
+    hasStravaUser.value = true
+
+    // check if token expires in next 60 minutes
+    const timeIn30Minutes = Number(new Date()) / 1000 + 3600
+    const expiresAt = Number(data.result.stravaUser.expires_at)
+    if (expiresAt < timeIn30Minutes) {
+      // todo refresh token
+      console.log("refresh token")
+    }
+
+    stravaAccessToken.value = data.result.stravaUser.accessToken
+    stravaRefreshToken.value = data.result.stravaUser.refreshToken
+  } else {
+    hasStravaUser.value = false
   }
-  if (refreshToken) {
-    stravaRefreshToken.value = refreshToken
-  }
-  if (accessToken) {
-    stravaAccessToken.value = accessToken
-  }
+  console.log(data)
+  console.log(userId.value)
+}
+
+onMounted(async () => {
+  await getUser()
+  updateUrl()
 })
 
 // todo make api request
-// todo refresh flow
 // todo how to store tokens
 
 </script>
@@ -52,15 +90,23 @@ onMounted(() => {
   <Header/>
   <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 border-2">
-      Auth Code: {{ stravaAuthCode }}
-      <br>
-      Refresh Token: {{ stravaRefreshToken }}
-      <br>
-      Access Token: {{ stravaAccessToken }}
-      <br>
-      <a v-bind:href="url" target="_blank" class="underline">Request access</a>
-      <br>
-      <button @click="makeRequest">Make Request</button>
+      <div v-if="!hasStravaUser">
+        You have not enabled Strava yet.
+        <a v-bind:href="theUrl" target="_blank" class="underline">Enable now?</a>
+      </div>
+      <div v-else>
+        Congrats, you have enabled Strava.
+        <br>
+        Refresh Token: {{ stravaRefreshToken }}
+        <br>
+        Access Token: {{ stravaAccessToken }}
+        <br>
+        <button @click="makeRequest" class="underline">Make Request</button>
+        <br>
+        <button @click="refreshStravaToken" class="underline">Refresh Strava Token</button>
+        <br>
+        <button @click="removeStrava" class="underline">Remove Strava</button>
+      </div>
     </div>
   </main>
 </template>
--- server/api/strava-callback.ts
+++ server/api/strava-callback.ts
@@ -1,12 +1,17 @@
 export default defineEventHandler(async (event) => {
-    const config = useRuntimeConfig()
+    const config = useRuntimeConfig().public
 
     const query = await getQuery(event)
     const authCode = query.code
 
-    setCookie(event, "runplanner-strava-auth-code", `${authCode}`)
+    console.log("UserId:", query.userId)
 
-    const res: {access_token: string | null, refresh_token: string | null} = await $fetch(
+    const res: {
+        access_token: string | null,
+        refresh_token: string | null,
+        expires_at: string | null,
+        athlete: {username: string | null}
+    } = await $fetch(
         "https://www.strava.com/oauth/token?" + new URLSearchParams({
             client_id: config.stravaClientId,
             client_secret: config.stravaClientSecret,
@@ -18,9 +23,21 @@ export default defineEventHandler(async (event) => {
 
     console.log(res)
 
-    if (res && res.access_token && res.refresh_token) {
-        setCookie(event, "runplanner-strava-access-token", `${res.access_token}`)
-        setCookie(event, "runplanner-strava-refresh-token", `${res.refresh_token}`)
+    if (res && res.access_token && res.refresh_token && res.expires_at && res.athlete.username) {
+        const stravaUser = await $fetch(
+            "/api/strava-user", {
+                method: "POST",
+                body: {
+                    userId: Number(query.userId),
+                    username: res.athlete.username,
+                    accessToken: res.access_token,
+                    refreshToken: res.refresh_token,
+                    expiresAt: Number(res.expires_at)
+                }
+            }
+        )
+
+        console.log(stravaUser)
     }
 
     return sendRedirect(event, '/integrations', 302)
